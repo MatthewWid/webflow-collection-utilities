a91527b447cadf172d12a4e6b7f586b1d40333bc
MatthewWid <matthew.widdi@gmail.com>
2020-07-23 23:46:28 +1000

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+const BASEDIR = path.resolve(__dirname);
+
+module.exports = (env) => ({
+	mode: env.production ? "production" : "development",
+	watch: !Boolean(env.production),
+	devtool: "inline-source-map",
+	entry: {
+		index: path.join(BASEDIR, "./src/index.ts"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	output: {
+		filename: "[name].js",
+		path: path.join(BASEDIR, "./build/"),
+	},
+});
